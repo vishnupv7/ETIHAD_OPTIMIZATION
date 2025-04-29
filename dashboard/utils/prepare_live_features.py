a6104a836952296782179dbc24b46ce675a454dd
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-29 15:41:07 +0530

    Update prepare_live_features.py

--- dashboard/utils/prepare_live_features.py
+++ dashboard/utils/prepare_live_features.py
@@ -6,4 +6,4 @@ def prepare_live_features(row):
         'wind_speed_kt': row.get('wind_speed_kt', 0),
         'expected_flight_duration_sec': row.get('expected_flight_duration_sec', 0),
         'deviation_flag': row.get('deviation_flag', 0),
-        'distance_penalty_km': row.get('distance_penalty_km', 0)
+        'distance_penalty_km': row.get('distance_penalty_km', 0) }
