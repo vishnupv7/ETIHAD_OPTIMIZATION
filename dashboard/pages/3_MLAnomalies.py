7847ebe83ec03d24592fec31a9bb9cd5a2288206
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-29 12:58:11 +0530

    Update 3_MLAnomalies.py

--- dashboard/pages/3_MLAnomalies.py
+++ dashboard/pages/3_MLAnomalies.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from utils.load_data import load_dashboard_data
 
@@ -7,7 +6,10 @@ df = load_dashboard_data()
 st.title("🧠 ML Anomalies Detection")
 
 anomalies = df[df['anomaly_score'] > 0.05]
-
 st.metric("Anomaly Flights Detected", len(anomalies))
 
-st.dataframe(anomalies[['callsign', 'anomaly_score', 'shap_top_feature']])
+if anomalies.empty:
+    st.success("🎉 No critical anomalies detected.")
+else:
+    st.warning("⚠️ Flights with unexpected CO₂ or Fuel Burn")
+    st.dataframe(anomalies[['callsign', 'anomaly_score', 'shap_top_feature', 'weather_adjusted_fuel_burn_kg', 'predicted_fuel_burn_kg']])
