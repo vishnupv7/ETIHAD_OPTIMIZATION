f56cd599d0bd7555322179b3cde1fa427365180f
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-29 12:58:35 +0530

    Update 4_WeatherAnalysis.py

--- dashboard/pages/4_WeatherAnalysis.py
+++ dashboard/pages/4_WeatherAnalysis.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import plotly.express as px
 from utils.load_data import load_dashboard_data
@@ -7,6 +6,9 @@ df = load_dashboard_data()
 
 st.title("🌧️ Weather Impact Analysis")
 
-fig = px.histogram(df, x='weather_penalty_index', nbins=20, title="Weather Penalty Distribution")
-
+fig = px.histogram(df, x='weather_penalty_index', nbins=20, title="Weather Penalty Index Distribution")
 st.plotly_chart(fig)
+
+st.subheader("🚨 Flights With High Weather Impact")
+weather_affected = df[df['weather_penalty_index'] > 1]
+st.dataframe(weather_affected[['callsign', 'weather_penalty_index', 'departure_weather']])
